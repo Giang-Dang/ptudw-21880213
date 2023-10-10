d91000b446fd0f9ec377ac9c185472858c3f916b
Dang Giang <dangvngiang@gmail.com>
2023-10-10 17:43:08 +0700

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const expressHandlebars = require('express-handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
