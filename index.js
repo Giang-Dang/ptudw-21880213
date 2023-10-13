094ceb8bcbf394ec368b4e14273f520afea0d963
Giang Dang <dangvngiang@gmail.com>
2023-10-13 22:31:20 +0700

    19

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const expressHandlebars = require('express-handlebars');
+const { createStarList } = require('./controllers/handlebarsHelper');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,16 +12,27 @@ app.engine('hbs', expressHandlebars.engine({
     layoutsDir: __dirname + '/views/layouts',
     partialsDir: __dirname + '/views/partials',
     extname: 'hbs',
-    defaultLayout: 'layout'
+    defaultLayout: 'layout',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    },
+    helpers : {
+        createStarList
+    }
 }));
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.use('/', require('./routes/indexRouter'));
+
+app.use('/products', require('./routes/productsRouter'));
+
+app.use((req, res, next) => {
+    res.status(404).render('error', { message: 'File not found!' });
 });
 
-app.get('/:page', (req, res) => {
-    res.render(req.params.page);
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).render('error', { message: 'Internal Server Error!' });
 });
 
 app.listen(port, () => {
